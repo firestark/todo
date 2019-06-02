2ef03dbf219f3b07b6a887472ff23d3137640605
Aron Wouters <lionar@mail.com>
2019-06-02 12:14:25 +0200

    Added complete action

--- client/views/todos/list/pending.blade.php
+++ client/views/todos/list/pending.blade.php
@@ -1,9 +1,20 @@
 @extends ( 'page.overview' )
 
 @section ( 'content' )
-    <ul class="mdc-list mdc-list--two-line mdc-list--linked">
+    <ul class="mdc-list mdc-list--two-line mdc-list--avatar-list mdc-list--highlight mdc-list--linked">
         @foreach ( $todos as $todo )
             <li class="mdc-list-item">
+                <a href="/{{ $todo->id }}/complete" 
+                    class="mdc-list-item__graphic" 
+                    aria-hidden="true">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="none" d="M0 0h24v24H0V0z"/>
+                        <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
+                    </svg>
+                </a>
+
+
                 <a href="/{{ $todo->id }}" style="position: relative">
                     <span class="mdc-list-item__text" style="width: 100%;">
                         <span class="mdc-list-item__primary-text">{{ $todo->description }}</span>
