c28435fd4032ff32d5346850bfab361acf4f24a3
Aron Wouters <lionar@mail.com>
2019-06-03 20:50:17 +0200

    Corrected redirect uri

--- client/statuses/1003. Removed todo.php
+++ client/statuses/1003. Removed todo.php
@@ -3,5 +3,5 @@
 status::matching ( 1003, function ( todo $todo )
 {
     session::flash ( 'message', 'Todo removed.' );
-    return redirect::to ( ( request::uri ( ) === '/completed' ) ? request::uri ( ) : '/' );
+    return redirect::to ( input::get ( 'up', '/' ) );
 } );
--- client/views/todos/list/completed.blade.php
+++ client/views/todos/list/completed.blade.php
@@ -4,7 +4,7 @@
     <ul id="completed-task-list" class="mdc-list mdc-list--two-line mdc-list--avatar-list mdc-list--highlight mdc-list--linked">
         @foreach ( $todos as $todo )
             <li class="mdc-list-item">
-                <a href="/{{ $todo->id }}/uncomplete" 
+                <a href="/{{ $todo->id }}/uncomplete?up=/completed" 
                     class="mdc-list-item__graphic" 
                     aria-hidden="true">
                     
@@ -22,7 +22,7 @@
                     </span>
                 </a>
 
-                <a href="/{{ $todo->id }}/remove" class="mdc-list-item__meta" aria-hidden="true">
+                <a href="/{{ $todo->id }}/remove?up=/completed" class="mdc-list-item__meta" aria-hidden="true">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
                         <path d="M0 0h24v24H0z" fill="none"/>
