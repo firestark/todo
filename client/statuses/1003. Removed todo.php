d3f2297d44e93c5eacf6f41250af667829b3e63a
Aron Wouters <lionar@mail.com>
2019-06-03 09:30:14 +0200

    Corrected redirect uri

--- client/statuses/1003. Removed todo.php
+++ client/statuses/1003. Removed todo.php
@@ -3,5 +3,5 @@
 status::matching ( 1003, function ( todo $todo )
 {
     session::flash ( 'message', 'Todo removed.' );
-    return redirect::to ( '/' );
+    return redirect::to ( ( request::uri ( ) === '/completed' ) ? request::uri ( ) : '/' );
 } );
