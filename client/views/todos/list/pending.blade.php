8a98f2c147822838255b337bc183e22fffe47fe8
Aron Wouters <lionar@mail.com>
2019-06-03 20:21:04 +0200

    Added empty list explanation text

--- client/views/todos/list/pending.blade.php
+++ client/views/todos/list/pending.blade.php
@@ -1,37 +1,57 @@
 @extends ( 'page.overview' )
 
 @section ( 'content' )
-    <ul id="pending-task-list" class="mdc-list mdc-list--two-line mdc-list--avatar-list mdc-list--highlight mdc-list--linked">
-        @foreach ( $todos as $todo )
-            <li class="mdc-list-item">
-                <a href="/{{ $todo->id }}/complete" 
-                    class="mdc-list-item__graphic" 
-                    aria-hidden="true">
-                    
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    @if ( count ( $todos ) )
+        <ul id="pending-task-list" class="mdc-list mdc-list--two-line mdc-list--avatar-list mdc-list--highlight mdc-list--linked">
+            @foreach ( $todos as $todo )
+                <li class="mdc-list-item">
+                    <a href="/{{ $todo->id }}/complete" 
+                        class="mdc-list-item__graphic" 
+                        aria-hidden="true">
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="none" d="M0 0h24v24H0V0z"/>
+                            <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
+                        </svg>
+                    </a>
+
+
+                    <a href="/{{ $todo->id }}" style="position: relative">
+                        <span class="mdc-list-item__text" style="width: 100%; display: grid; padding-right: 16px;">
+                            <span class="mdc-list-item__primary-text">{{ $todo->description }}</span>
+                            <span class="mdc-list-item__secondary-text">Created at {{  date ( 'M d, Y ', $todo->createdAt ) }}</span>
+                        </span>
+                    </a>
+
+                    <a href="/{{ $todo->id }}/remove" class="mdc-list-item__meta" aria-hidden="true">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                        </svg>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    @else
+        <div style="text-align: center; margin: 56px auto 0; width: 230px;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24" fill="var(--mdc-theme-primary)" style="opacity: 0.87;">
+                <path fill="none" d="M0 0h24v24H0V0z"/>
+                <path opacity=".3" d="M12.01 18c-1.48 0-2.75-.81-3.45-2H5v3h14v-3h-3.55c-.69 1.19-1.97 2-3.44 2z"/>
+                <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5v-3h3.56c.69 1.19 1.97 2 3.45 2s2.75-.81 3.45-2H19v3zm0-5h-5c0 1.1-.9 2-2 2s-2-.9-2-2H5V5h14v9z"/>
+            </svg>
+
+            <h3 class="mdc-typography--subtitle2" style="font-weight: bold; margin: 8px 0 0;">No todos here</h3>
+            <p class="mdc-typography--body2" style="color: var(--mdc-theme-text-secondary-on-background); margin: 0; line-height: 1.5rem;">Add a todo with the
+                <span style="vertical-align: text-top;"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: var(--mdc-theme-text-secondary-on-background); position: relative; top: 4px;">
                         <path fill="none" d="M0 0h24v24H0V0z"/>
-                        <path d="M9 16.17L5.53 12.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.18 4.18c.39.39 1.02.39 1.41 0L20.29 7.71c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L9 16.17z"/>
+                        <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"/>
                     </svg>
-                </a>
+                </span> button
+            </p>
+        </div>
+    @endif
 
-
-                <a href="/{{ $todo->id }}" style="position: relative">
-                    <span class="mdc-list-item__text" style="width: 100%; display: grid; padding-right: 16px;">
-                        <span class="mdc-list-item__primary-text">{{ $todo->description }}</span>
-                        <span class="mdc-list-item__secondary-text">Created at {{  date ( 'M d, Y ', $todo->createdAt ) }}</span>
-                    </span>
-                </a>
-
-                <a href="/{{ $todo->id }}/remove" class="mdc-list-item__meta" aria-hidden="true">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-                        <path d="M0 0h24v24H0z" fill="none"/>
-                    </svg>
-                </a>
-            </li>
-        @endforeach
-    </ul>
-    
     @include ( 'partials.link.fab', [ 'action' => 'add', 'link' => '/add' ] )
 @endsection
 
